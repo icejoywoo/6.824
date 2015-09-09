5c7ba537978c793072d115c2ad5aad642c967dee
wujiabin <wujiabin@baidu.com>
2015-09-09 15:20:24 +0800

    update lab1 part-III

--- src/mapreduce/master.go
+++ src/mapreduce/master.go
@@ -32,81 +32,105 @@ func (mr *MapReduce) RunMaster() *list.List {
 	// Your code here
 	mapChan := make(chan int, mr.nMap)
 	defer close(mapChan)
+	mapFailedChan := make(chan int, mr.nMap)
+	defer close(mapFailedChan)
 
-	// map
-	for i := 0; i < mr.nMap; i++ {
-		go func(index int) {
-			// 运行失败会直接重试，死循环直到成功
-			for {
-				args := DoJobArgs{
-					File: mr.file,
-					Operation: Map,
-					JobNumber: index,
-					NumOtherPhase: mr.nReduce,
-				}
-				var reply DoJobReply
-				var worker string
-				var ok bool = false
-				select {
-				case worker = <-mr.registerChannel:
-					DPrintf("map worker: %v\n", worker)
-					ok = call(worker, "Worker.DoJob", args, &reply)
-				case worker = <-mr.idleChannel:
-					DPrintf("map worker: %v\n", worker)
-					ok = call(worker, "Worker.DoJob", args, &reply)
-				}
+	var map_func func(int)
+	map_func = func(index int) {
+		args := DoJobArgs{
+			File: mr.file,
+			Operation: Map,
+			JobNumber: index,
+			NumOtherPhase: mr.nReduce,
+		}
+		var reply DoJobReply
+		var worker string
+		var ok bool = false
+		select {
+		case worker = <-mr.registerChannel:
+			DPrintf("map worker: %v\n", worker)
+			ok = call(worker, "Worker.DoJob", args, &reply)
+		case worker = <-mr.idleChannel:
+			DPrintf("map worker: %v\n", worker)
+			ok = call(worker, "Worker.DoJob", args, &reply)
+		}
 
-				if ok {
-					mapChan <- index
-					mr.idleChannel <- worker
-					return
-				}
-			}
-		} (i)
+		if ok {
+			mapChan <- index
+		} else {
+			mapFailedChan <- index
+			DPrintf("Failed worker: %v, retrying\n", worker)
+		}
+		// worker归还到idle中
+		mr.idleChannel <- worker
 	}
 
 	for i := 0; i < mr.nMap; i++ {
-		<- mapChan
+		go map_func(i)
+	}
+
+	mapSuccess := 0
+	for mapSuccess < mr.nMap {
+		var index int
+		select {
+		case index = <-mapChan:
+			DPrintf("map index: %v, success: %v\n", index, mapSuccess)
+			mapSuccess++
+		case index = <-mapFailedChan:
+			go map_func(index)
+		}
 	}
 
 	DPrintf("Map is done.")
 
 	reduceChan := make(chan int, mr.nReduce)
 	defer close(reduceChan)
+	reduceFailedChan := make(chan int, mr.nReduce)
+	defer close(reduceFailedChan)
 
-	for i := 0; i < mr.nReduce; i++ {
-		go func(index int) {
-			// 运行失败会直接重试，死循环直到成功
-			for {
-				args := DoJobArgs{
-					File: mr.file,
-					Operation: Reduce,
-					JobNumber: index,
-					NumOtherPhase: mr.nMap,
-				}
-				var reply DoJobReply
-				var worker string
-				var ok bool = false
-				select {
-				case worker = <-mr.registerChannel:
-					DPrintf("reduce worker: %v\n", worker)
-					ok = call(worker, "Worker.DoJob", args, &reply)
-				case worker = <-mr.idleChannel:
-					DPrintf("reduce worker: %v\n", worker)
-					ok = call(worker, "Worker.DoJob", args, &reply)
-				}
+	var reduce_func func(int)
+	reduce_func = func(index int) {
+		args := DoJobArgs{
+			File: mr.file,
+			Operation: Reduce,
+			JobNumber: index,
+			NumOtherPhase: mr.nMap,
+		}
+		var reply DoJobReply
+		var worker string
+		var ok bool = false
+		select {
+		case worker = <-mr.registerChannel:
+			DPrintf("reduce worker: %v\n", worker)
+			ok = call(worker, "Worker.DoJob", args, &reply)
+		case worker = <-mr.idleChannel:
+			DPrintf("reduce worker: %v\n", worker)
+			ok = call(worker, "Worker.DoJob", args, &reply)
+		}
 
-				if ok {
-					reduceChan <- index
-					mr.idleChannel <- worker
-					return
-				}
-			}
-		} (i)
+		if ok {
+			reduceChan <- index
+		} else {
+			reduceFailedChan <- index
+			DPrintf("Failed worker: %v, retrying", worker)
+		}
+		mr.idleChannel <- worker
 	}
 
 	for i := 0; i < mr.nReduce; i++ {
-		<- reduceChan
+		go reduce_func(i)
+	}
+
+	reduceSuccess := 0
+	for reduceSuccess < mr.nReduce {
+		var index int
+		select {
+		case index = <-reduceChan:
+			DPrintf("reduce index: %v, success: %v\n", index, mapSuccess)
+			reduceSuccess++
+		case index = <-reduceFailedChan:
+			go reduce_func(index)
+		}
 	}
 
 	DPrintf("reduce done\n")
